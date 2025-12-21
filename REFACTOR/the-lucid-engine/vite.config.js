c71bbd220840364bfab7956bf09faa18a1b6e287
XxZIOIMIBIExX <personal.encryption@aleeas.com>
2025-12-21 12:26:45 -0800

    fixing stupid gitignore errors

--- REFACTOR/the-lucid-engine/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace "acidgreenservers" with your GitHub username if needed
-export default defineConfig({
-    plugins: [react()],
-    base: '/the-lucid-engine/',
-})
